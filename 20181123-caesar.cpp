1d295bdb7e8b48e8844fae0ac80b32b746510bf1
小克 <goodjack@users.noreply.github.com>
2018-12-07 00:10:33 +0800

    Create 20181123-caesar.cpp

---
+++ 20181123-caesar.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+string caesar(int key, string input);
+
+int main() {
+    int key;
+    cout << "請輸入密鑰：";
+    cin >> key;
+
+    string plaintext;
+    cout << "請輸入明文：";
+    cin >> plaintext;
+
+    string ciphertext = caesar(key, plaintext);
+
+    cout << "密文為：" << ciphertext;
+}
+
+string caesar(int key, string plaintext) {
+    for(int i = 0; plaintext[i] != '\0'; i++) {
+        // 大寫
+        if (plaintext[i] >= 'A' && plaintext[i] <= 'Z') {
+            plaintext[i] += key;
+            if (plaintext[i] > 'Z') {
+                plaintext[i] -= 26;
+            }
+        } else if (plaintext[i] >= 'a' && plaintext[i] <= 'z') {
+            plaintext[i] += key;
+            if (plaintext[i] > 'z') {
+                plaintext[i] -= 26;
+            }
+        }
+    }
+
+    return plaintext;
+}
