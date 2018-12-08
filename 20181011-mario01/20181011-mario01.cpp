bcf930b7a203238b7fdf0e07dac98caba0067132
Jack <jack_lin@intentarget.com>
2018-12-08 10:22:18 +0800

    Add the Mario01 solution

---
+++ 20181011-mario01/20181011-mario01.cpp
@@ -0,0 +1,28 @@
+// 馬力歐三角形 1：基本版靠右三角形
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+
+    do {
+        cout << "Height: ";
+        cin >> height;
+    } while (height < 1 || height > 8); // 檢查使用者輸入的高度範圍為 1~8
+
+
+    for (int i = 1; i <= height; i++) {
+        // 各行的空白個數 == 高度 - 第幾行
+        for (int space = 0; space < height - i; space++) {
+            cout << " ";
+        }
+
+        // 星號個數 == 第幾行
+        for (int star = 0; star < i; star++) {
+            cout << "*";
+        }
+
+        // 輸出完每一行的東西要換行
+        cout << "\n";
+    }
+}
